935e1c082b3cc286147f4b869895e0a04eebf982
Sabir Kirpal <57548850+kingkirpal2000@users.noreply.github.com>
2021-10-31 13:05:46 -0700

    Update Communicator.java

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -1,41 +1,50 @@
 package nachos.threads;
-
 import nachos.machine.*;
 
-/**
- * A <i>communicator</i> allows threads to synchronously exchange 32-bit
- * messages. Multiple threads can be waiting to <i>speak</i>,
- * and multiple threads can be waiting to <i>listen</i>. But there should never
- * be a time when both a speaker and a listener are waiting, because the two
- * threads can be paired off at this point.
- */
-public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
-    public Communicator() {
-    }
 
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
-    public void speak(int word) {
-    }
+public class communicator{
+	private int r;
+	private Lock mutex;
+	private Condition speakReady;
+    private Condition listenReady; 
+    private int listener;
+    private int speaker;
 
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	return 0;
-    }
+    //Allocate a communicator 
+	public communicator{
+		mutex = new lock();
+		listenerReady = new Condition(lock);
+		speakerReady = new Condition(lock);
+		listener = 0;
+		speaker = 0;
+	}
+
+	public void speak(int word){
+		mutex.acquire();
+		speaker++;
+		while(speaker==1){
+			speakerReady.sleep();
+		}
+		while(listener==0){
+			listenerReady.wake();
+		}
+		this.r = word;
+		mutex.release();
+	}
+
+	public int listen(){
+		mutex.acquire();
+		listener++:
+		if(listen == 1){
+			listenerReady.sleep();
+		}
+		if(speaker == 0){
+			speakerReady.wake();
+		} 
+		listener--;
+		mutex.release();
+		return this.r;
+	}
 }
+	
+
